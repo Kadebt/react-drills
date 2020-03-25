848162d96ea0115ea6790d7bb02028e397c7732f
Kade Thompson <kadebthompson3@gmail.com>
2020-03-25 16:59:08 -0600

    unfinished react2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor(){
+    super();
 
+    this.state = {
+      arr: ['spaghetti', 'ice cream', 'sushi', 'bologna', 'cheese']
+    }
+  }
+
+  render() {
+    let arrDisplay = this.state.arr.map((e, i) => {
+      return <h2 key={i}>{e}</h2>;
+    });
+
+    return <div className="App">{arrDisplay}</div>;
+  }
+}
+  
 export default App;
--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      newarr:[''],
+      arr: ['spaghetti', 'ice cream', 'sushi', 'bologna', 'cheese']
+    }
+  }
+    handlerText(val){
+      this.setState({newarr: val})
+    }
+    render(){
+      let arrfilter = this.state.arr.filter((e, i) => {
+        return e.includes(this.state.newarr)
+      })
+      .map((e,i) => {
+        return <h2 key={i}>{e}</h2>
+      })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input onChange={e => this.handlerText(e.target.value)} type='text'/>
+      {arrfilter}
     </div>
   );
 }
+}
+  
 
 export default App;
