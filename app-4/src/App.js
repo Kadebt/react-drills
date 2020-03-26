9a439e9200bb6f391140089691f658df75bdae04
Kade Thompson <kadebthompson3@gmail.com>
2020-03-26 14:17:36 -0600

    react-drills

--- app-4/src/App.js
+++ app-4/src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component{
-  
-}
+import Login from "./Login";
 
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Login />
+      </div>
+    );
+  }
+}
 export default App;
